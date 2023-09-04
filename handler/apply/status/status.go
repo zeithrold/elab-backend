e862958d0b5404ad5853ef0becd0a774bd5f76bd
zeithrold <zeithrold@gmail.com>
2023-09-04 16:27:06 +0800

    implemented textform lock on getstatus

--- handler/apply/status/status.go
+++ handler/apply/status/status.go
@@ -2,6 +2,7 @@ package status
 
 import (
 	"elab-backend/model/apply"
+	"elab-backend/service/redis"
 	"elab-backend/util/auth"
 	"github.com/gin-gonic/gin"
 )
@@ -14,5 +15,12 @@ func ApplyRoute(group *gin.RouterGroup) {
 func GetStatus(ctx *gin.Context) {
 	token := auth.GetToken(ctx)
 	openid := token.RegisteredClaims.Subject
+	unlock, err := redis.GetLock(ctx, "textform:"+openid)
+	if err != nil {
+		ctx.AbortWithStatusJSON(500, gin.H{
+			"message": "服务器错误",
+		})
+	}
+	defer unlock()
 	ctx.JSON(200, apply.GetStatus(ctx, openid))
 }
